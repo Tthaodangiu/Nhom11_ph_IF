8e4bb8802c81d705331e847c7237b9f15db66e8e
Tthaodangiu <150150128+Tthaodangiu@users.noreply.github.com>
2023-11-07 07:33:46 -0800

    Add files via upload

---
+++ cau40.py
@@ -0,0 +1,12 @@
+Canh_1=float(input(""))
+Canh_2=float(input(""))
+Canh_3=float(input(""))
+if Canh_1==Canh_2==Canh_3:
+    print("Tam giac deu")
+elif Canh_1==Canh_2!=Canh_3 or Canh_1==Canh_3!=Canh_2 or Canh_2==Canh_3!=Canh_1:
+    print("Tam giac can")
+else:
+    print("Tam giac thuong")
+
+    
+    
